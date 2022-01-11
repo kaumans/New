f4006003cee7e79106bf6fecfd917c63148236f6
kaumans <97483430+kaumans@users.noreply.github.com>
2022-01-11 10:21:52 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,11 @@ input.onGesture(Gesture.Shake, function () {
     basic.showNumber(12)
 })
 basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
+    # # . . .
+    . # # # .
+    . . . # .
+    . . . # #
+    . . . . #
     `)
 basic.forever(function () {
 	
