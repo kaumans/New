78acd7160f77fc0d9af23bf017f543813aba74c9
kaumans <97483430+kaumans@users.noreply.github.com>
2022-01-11 10:19:55 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(12)
+})
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
