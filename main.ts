40074432da74d0d8a95bb2c74c8b516124b7018d
kaumans <97483430+kaumans@users.noreply.github.com>
2022-09-05 21:02:21 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(20)
 })
+input.onGesture(Gesture.LogoUp, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(12)
 })
